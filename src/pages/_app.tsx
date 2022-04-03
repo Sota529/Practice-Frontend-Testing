c80a988c54d49b0f900707e0cbd3f6f2ec2827f9
Sota Hirose <za446131@gmail.com>
2022-04-04 04:54:15 +0900

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import 'normalize.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
